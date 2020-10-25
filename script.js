4bd1dbc5d0a760d54088f7c91ca19301ad7c3b74
Stephen Domican <57364979+Domo2304@users.noreply.github.com>
2020-10-25 09:38:53 +0000

    Add files via upload
    Chart JS now updating

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var ctx = document.getElementById('myChart');
 
-function calculateSavings(savings){
+function calculateSavings(savings) {
 
     let savingsData = new Map();
     let totalSavings = 0;
-    for(let i = 0; i < 10; i++){
-        let year = 2020+i;
+    for (let i = 0; i < 10; i++) {
+        let year = 2020 + i;
         totalSavings += savings;
-        savingsData.set(totalSavings, year);
+        savingsData.set(year, totalSavings);
     }
-
+    //console.log("savingsData: " + JSON.stringify(savingsData));
     return savingsData;
 }
 
@@ -19,35 +19,67 @@ let savingsArray = [];
 let yearArray = [];
 
 //Adds values from map to an array.
-for(let value of testMap.keys()){
+for (let value of testMap.values()) {
+    //console.log("Pushing savings of " + value);
     savingsArray.push(value);
 }
 
 //add keys from map(years) to an array
-for(let key of testMap.keys()){
+for (let key of testMap.keys()) {
+    //console.log("Pushing year of " + key);
     yearArray.push(key);
 }
 
+console.log("Initial year array: " + yearArray);
 
-//var stars = [135850, 52122, 148825, 16939, 9763];
-//var frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
 
 var myChart = new Chart(ctx, {
     //type: 'bar',
     type: "line",
     data: {
-       labels: yearArray,
-        //labels: frameworks,
-       datasets: [{
-
-           label: 'Savings',
-           data: savingsArray
-        //    data: stars
-           }]
+        labels: yearArray,
+        datasets: [{
+            label: 'Savings',
+            data: savingsArray
+        },{
+            label: 'My second dataset (test)',
+            //backgroundColor: "rgb(0,200,0)",
+            borderColor: "rgb(0,99,0)",
+            data: savingsArray
+        }
+    
+    ]
     },
-   });
+});
 
-   console.log(savingsArray);
-   console.log(yearArray);
 
-   //Testing Git Push....
+//function for updating Chart.JS
+// function updateChart(){
+//     myChart.data.datasets[0].data = [100, 200, 300, 400,500,600,700,800,900,1000]
+//     myChart.update(); 
+// }
+
+
+function updateChart(savings) {
+
+    let newMap = new Map();
+    newMap = calculateSavings(savings);
+    let newSavingsArray = [];
+    let yearArray = [];
+
+    //Adds values from map to an array.
+    for (let value of newMap.values()) {
+        newSavingsArray.push(value);
+    }
+
+    //add keys from map(years) to an array
+    for (let key of newMap.keys()) {
+        yearArray.push(key);
+    }
+    // console.log("Year Array: " + yearArray);
+    // console.log(savings);
+    // console.log(newSavingsArray);
+    // console.log(myChart.data.datasets[0].data);
+    myChart.data.datasets[0].data = newSavingsArray;
+    myChart.update();
+}
